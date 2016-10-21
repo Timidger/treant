e8b027975198f9af415c432dfe94527903e45c18
preston <preston@redstar.com>
2016-10-21 11:11:14 -0400

    Added docs and deny lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! A library of trees, each with different trade offs and benefits.
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_import_braces, unused_qualifications
+)]
 mod binary;
 
 #[cfg(test)]
